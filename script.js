64d22aee32e5a0518ebe24ca0ee5ad89ae4ea184
Roin Putkaradze <putkaradzeroin1@gmail.comm>
2020-11-11 18:50:51 +0400

    newSlide

--- script.js
+++ script.js
@@ -1,64 +1,56 @@
 const imageContainer = document.getElementById("image");
-const imagesContainer = document.getElementById("images");
 const buttonsContainer = document.getElementById("buttons");
-const imageElement = document.createElement("img");
+const transitionButton = document.getElementById("tr-button");
 let index = 1;
-const second = 1000;
- 
+let second = transitionButton.value;
+let interval;
 
-const imageSrcs = [
-    "./images/acmilan.jfif",
-    "./images/gatusso.jpg",
-    "./images/kaka.jfif",
-    "./images/maldini.jfif",
-    "./images/images.jfif",
-    "./images/league.jpg" ,
-    "./images/tokatayajpg.jpg"   
-];
 
-imageElement.setAttribute("src", imageSrcs[0]);
-imageContainer.appendChild(imageElement);
-
-for(let i = 0; i < imageSrcs.length; i++){
-    const imageElement = document.createElement("img");
-    const buttonElement = document.createElement("div");
-    buttonElement.id = i;
-    imagesContainer.appendChild(imageElement);
-    imageElement.setAttribute("src", imageSrcs[i]);
-    buttonsContainer.appendChild(buttonElement);
+function startInterval(){
+    interval = setInterval(changeImage, second);
 }
 
-buttonsContainer.children[0].classList.add("active");
-
-
-function changeImage(){
-    imageElement.setAttribute("src",imageSrcs[index]);
+function changeImage(){ 
     document.getElementsByClassName("active")[0].classList.remove("active");
-
     buttonsContainer.children[index].classList.add("active");
-    index++;
-    if(index == imageSrcs.length){
+    document.getElementsByClassName("show")[0].classList.remove("show");
+    imageContainer.children[index].classList.add("show")
+    index++;    
+    if(index >= imageContainer.children.length){
         index = 0;
     }
 }
 
-let interval = setInterval(changeImage, second);
 
-imageElement.addEventListener("mouseenter",function(){
+imageContainer.addEventListener("mouseenter",function(){
     clearInterval(interval);
 })
 
-
-imageElement.addEventListener("mouseleave",function(){
-    interval = setInterval(changeImage, second);
+imageContainer.addEventListener("mouseleave",function(){
+    startInterval()
 });
-
 
 buttonsContainer.addEventListener("click",function(e){
     if(e.target != buttonsContainer){
         clearInterval(interval);
         index = +e.target.id
         changeImage();
-        interval = setInterval(changeImage, second);
+        startInterval();
     }
-})
+});
+
+transitionButton.addEventListener("change",function(){
+    clearInterval(interval);
+    second = +transitionButton.value;
+    startInterval();    
+});
+
+for(let i = 0; i < imageContainer.children.length; i++){
+    const buttonElement = document.createElement("div");
+    buttonElement.id = i;
+    buttonsContainer.appendChild(buttonElement);
+}
+
+buttonsContainer.children[0].classList.add("active");
+
+startInterval();
